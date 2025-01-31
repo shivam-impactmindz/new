3c70d8e68a107f68413169117f5adf04c0c7c149
shivam-impactmindz <shivam.impactmindz@gmail.com>
2025-01-31 13:52:32 +0530

    update

--- utils/database.js
+++ utils/database.js
@@ -1,9 +1,6 @@
 import mongoose from "mongoose";
 
 const connectToDatabase = async () => {
-  if (mongoose.connection.readyState >= 1) {
-    return;
-  }
   await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
